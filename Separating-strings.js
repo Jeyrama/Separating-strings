6fdba33d0549863cab7cf61c28183fb8978baca0
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-07-20 13:40:23 -0700

    Create Separating-strings.js

---
+++ Separating-strings.js
@@ -0,0 +1,17 @@
+/*
+Create a function that takes a string and separates it into a sequence of letters.
+Examples:
+  sepStr("Just Live Life Man")
+  [['J','L','L','M'],
+  ['u','i','i','a'],
+  ['s','v','f','n'],
+  ['t','e','e','']]);
+
+The function should separate each word into individual letters, 
+with the first word in the sentence having its letters in 
+the 0th index of each 2nd dimension array, and so on.
+
+Shorter words will have an empty string in the 
+place once the word has already been mapped out. 
+(See the last element in the last part of the array.)
+*/
