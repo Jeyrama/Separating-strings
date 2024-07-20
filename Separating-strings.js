608219e92de532d3fa1fca1f6307c3c90e06ea55
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-07-20 14:52:36 -0700

    Update Separating-strings.js

--- Separating-strings.js
+++ Separating-strings.js
@@ -31,3 +31,35 @@ function sepStr(arr) {
 }
 
 // or
+
+function sepStr(str) {
+  let sepArr = [];
+  let words = str.split(' ');
+  
+  // find longest word to know how many times to repeat cycle
+  let longestLen = 0;
+  for (let word of words) {
+    if (word.length > longestLen) {
+      longestLen = word.length;
+    }
+  }
+  
+  // iterate over the array of words however many times to get every letter of the longest word
+  for (let i = 0; i < longestLen; i++) {
+    // make a new array for each row / index
+    let rowStr = [];
+    for (let word of words) {
+      // if haven't gone past the length of a word, add the letter at that index to the array and remove it from the word
+      if (word[i] !== undefined) {
+        rowStr.push(word[i]);
+      } else {
+        // if we have gone past the length of a word, push in an empty string
+        rowStr.push('');
+      }
+    }
+    // add the array of letters to the whole array
+    sepArr.push(rowStr);
+  }
+  
+  return sepArr;
+}
